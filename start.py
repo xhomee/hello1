3c535726ba49d24734d6b0dd4a49ae2011efb5e2
xhomee <xhomee@gmail.com>
2023-01-18 23:53:36 +0200

    end first day

--- start.py
+++ start.py
@@ -1,10 +1,11 @@
-print("hello world")
-
-a= 10
-b = 14
-if a >= 11:
-    print(a+b)
-else:
-    print(b)
+a = input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?\n")
+b = input("What's your pet's name?\n")
+print("Your band name could be " + a + ' ' + b)
 
 
+# Welcome to the Band Name Generator.
+# What's the name of the city you grew up in?
+# dfg
+# What's your pet's name?
+# wertwer
+# Your band name could be df df
