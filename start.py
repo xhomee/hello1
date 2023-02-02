0affca5b7fe0268dfb3f05caf0855abae41a4bfb
xhomee <xhomee@gmail.com>
2023-02-02 08:10:48 +0200

    8 day end cryptographer

--- start.py
+++ start.py
@@ -1,52 +1,53 @@
-import random
-import pi_module
+from pi_module import logo
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
 
-word_list = ["ardvark", "baboon", "camel"]
-#pi_module.world_list
-chosen_word = random.choice(word_list)
-lives = 6
-end_of_game = False
-
-print(pi_module.logo)
-
-# NEED add _ in list
-add_underscore = []
-for letters in chosen_word:
-    add_underscore.append("_")
-
-
-while not end_of_game:
-    while not lives <= 0:
-        guess = input("Enter a letter ").lower()  # original
-        if guess in add_underscore:
-            print("This letter already exists")
-            print("")
-
-        elif guess in chosen_word:
-            for position in range(len(chosen_word)):
-                letter = chosen_word[position]
-                if letter == guess:
-                    add_underscore[position] = letter
-            break
-
+def caesar(text_t, shift_t, direction_t):
+    end_text = ""
+    for letter in text_t:
+        if letter not in alphabet:
+            end_text += letter
         else:
-            lives -=1
-            print(f"{guess} not in the word")
-            break
+            position = alphabet.index(letter)
+            if direction_t == "encode" or direction_t == "e":
+                if position + shift_t > 25:
+                    new_position = (position + shift_t) - 26
+                else:
+                    new_position = position + shift_t
+            elif direction_t == "decode" or direction_t == "d":
+                if position - shift_t < 0:
+                    new_position = (26 + (position - shift_t))
+                else:
+                    new_position = position - shift_t
+            else:
+                print("I dont know this comand")
 
-
-    print(pi_module.stages[lives])
-    print(f"{' '.join(add_underscore)}    word length: {len(chosen_word)}")
+            new_letter = alphabet[new_position]
+            end_text +=new_letter
+    print(f"The {direction_t} text is: {end_text}")
     print("")
 
-    if lives <= 0:
-        end_of_game = True
-        print("Your lose")
 
-    if '_' not in add_underscore:
-        end_of_game = True
-        print("You win")
-
+code_working = True
+while code_working:
+    direction = input("Type 'encode or e' to encrypt, type 'decode or d' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
 
+    if shift > 26 :
+        x = shift % 26
+    else:
+        x = shift
+    caesar(text_t=text, shift_t=x, direction_t=direction)
+
+    end = input("Type 'yes or y' if you want to go again. Otherwise type 'no or n'.\n").lower()
+    if end == "yes" or end == "y":
+        pass
+    elif end == "xhomee":
+        print("Your a god")
+        pass
+    else:
+        code_working = False
+        print("Good Bye")
 
