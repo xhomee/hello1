2a8e573a10a0814de3c336e92027f3c12592694b
xhomee <xhomee@gmail.com>
2023-04-09 19:10:13 +0300

    15 day test

--- start.py
+++ start.py
@@ -1,68 +1,89 @@
-from game_data import data
-from pi_module import vs, higher_lower
-import random
-
-user_score = 0
-game_on = True
+from game_data import MENU, resources
+# TODO create first
 
 
-def randoms():
-    random_answer = random.choice(data)
-    random_name = random_answer['name']
-    random_count = random_answer['follower_count']
-    random_description = random_answer['description']
-    random_country = random_answer['country']
-    return random_answer, random_name, random_count, random_description, random_country
+#print(MENU["latte"]["ingredients"]["water"])
 
 
-random_answer, random_name, random_count, random_description, random_country = randoms()
-random_answer2, random_name2, random_count2, random_description2, random_country2 = randoms()
+def report():
+    #print(resources)
+    water = resources["water"]
+    coffee = resources["coffee"]
+    milk = resources["milk"]
+    return water, coffee, milk
+
+water, coffee, milk = report()
+print(report())
 
 
-def check_who_biggest():
-    global user_score
-    if random_count > random_count2:
-        return random_count
-    elif random_count2 > random_count:
-        return random_count2
-    else:
-        user_score += 1
-        return random_count2
+
+def coins(how):
+    quarters = int(input("how many quarters?"))
+    return quarters * how
 
 
-print(higher_lower)
-a_print =(f'Answer A: {random_name}, a {random_description}, from {random_country} \n'
-          f'{vs}\nAnswer B: {random_name2}, a {random_description2}, from {random_country2} ')
-print(a_print)
+def show_element(element):
+    """return list elements coffee"""
+    element_list = []
+    ingredients = MENU[element]["ingredients"]
+    for key in ingredients:
+        #element_list.append(key)
+        element_list.append(ingredients[key])
+    element_list.append(MENU[element]["cost"])
+    #print(element_list)
+    return element_list
 
 
-while game_on:
-    answer = input("\nWho has more followers? Type 'A' or 'B': ")
-    if answer == "a" or answer == "A":
-        if random_count == check_who_biggest():
-            user_score += 1
-            print(f"\nYou score: {user_score}\n")
-            random_answer2, random_name2, random_count2, random_description2, random_country2 = randoms()
-            print(f'\nAnswer A: {random_name}, a {random_description}, from {random_country}')
-            print(vs)
-            print(f'Answer B: {random_name2}, a {random_description2}, from {random_country2}')
-        else:
-            input(f"You lose. you score {user_score}")
-            game_on = False
-    elif answer == "b" or answer == "B":
-        if random_count2 == check_who_biggest():
-            user_score += 1
-            print(f"\nYou score: {user_score}\n")
-            random_answer, random_name, random_count, random_description, random_country = randoms()
-            print(f'\nAnswer A: {random_name}, a {random_description}, from {random_country}')
-            print(vs)
-            print(f'Answer B: {random_name2}, a {random_description2}, from {random_country2}')
-        else:
-            input(f"You lose. you score {user_score}")
-            game_on = False
-    elif answer == "xhome":
-        user_score += 999
-        print(f"\nYou score: {user_score}\n")
-    else:
-        input(f"You lose. you score {user_score}")
-        game_on = False
+def check_resources(report, element_list):
+    new_water = water-element_list[0]
+    new_coffee = coffee-element_list[1]
+
+    print(new_water)
+    print(new_coffee)
+    #report[0] -= element_list[0]
+
+    #print(len(element_list))
+    print(element_list) #['water', 250, 'coffee', 24, 'milk', 100, 2.5] [250, 24, 100, 3.0]
+    print(report)#(300, 200, 100) (300, 100, 200)
+    print("dsfg")
+
+
+
+# print(show_element('espresso'))
+# print(" ")
+# print(show_element('latte'))
+
+ask_user = input("What would you like? (espresso/latte/cappuccino): ").lower()
+#print(show_element(ask_user))
+
+check_resources(report(), show_element(ask_user))
+# for key in MENU:
+#     print(key)
+#     print(MENU[key]['ingredients']['water'])
+
+#
+# ask_user = input("What would you like? (espresso/latte/cappuccino): ").lower()
+#
+# #show_element(ask_user)
+# key, ingredients_key, cost = show_element(ask_user)
+# print(key, ingredients_key, cost)
+# report()
+
+
+
+
+
+# "latte": {
+#         "ingredients": {
+#             "water": 200,
+#             "milk": 150,
+#             "coffee": 24,
+#         },
+#         "cost": 2.5,
+#     },
+
+# resources = {
+#     "water": 300,
+#     "milk": 200,
+#     "coffee": 100,
+# }
